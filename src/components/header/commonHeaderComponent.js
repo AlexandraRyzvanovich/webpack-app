faf3f5cf5b7c8e1908a6e5420b6abfdfaa88c0d5
Aliaksandra Ryzvanovich <aliaksandra.ryzvanovich@epam.com>
2021-10-10 23:21:51 +0300

    refactoring

---
+++ src/components/header/commonHeaderComponent.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+import SearchComponent from "./searchComponent";
+import LogoComponent from "./logoComponent";
+
+function CommonHeader(props) {
+  return (
+    <>
+      <header className="header">
+        <LogoComponent />
+        <button type="submit" className="button-add" onClick={props.onOpen}>
+          <p>+ ADD MOVIE</p>
+        </button>
+        <SearchComponent onSearch={props.onSearch} />
+      </header>
+    </>
+  );
+}
+
+CommonHeader.propTypes = {
+  onOpen: PropTypes.func.isRequired,
+  onOpenSearch: PropTypes.func,
+};
+
+export default CommonHeader;
